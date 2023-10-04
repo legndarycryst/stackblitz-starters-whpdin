6615759f7137c34dd6de7a374996aaae2b26c45b
legndarycryst <119542965+legndarycryst@users.noreply.github.com>
2023-10-04 08:44:06 -0400

    1 file modified

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 
 console.log(`Hello Node.js v${process.versions.node}!`);
 
+const express = require('express');
+const app = express();
+const port = 25566; // Replace with your desired port number
+//3000
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
